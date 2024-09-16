b8f7d1ff4c89db7b1115b09839985229b9b46ed3
Timon Rieger <160211096+timonrieger@users.noreply.github.com>
2024-09-16 15:30:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#pip install ccxt
-
 import json
 import ccxt
 from ccxt.base.errors import BadSymbol
@@ -11,17 +9,6 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import os
 import matplotlib.cm as cm
-
-### Disclaimer ###
-# This script is provided for informational purposes only and should not be considered financial advice.
-# You should not run this code. It is just for viewing to evaluate the accuracy of the study.
-
-### Important Notes ###
-
-# The script lacks comprehensive error handling and may fail under certain conditions.
-# It is designed to work with data from a specific Telegram channel and may not be compatible with data from other sources due to the wide variety of message formats.
-# The format_data function must be adapted to suit the specific structure of your data input.
-
 
 # Takes the Telegram JSON file and parses it
 def format_data():
